fada3819abc7dee4babd1e90a0812fed69effeca
Tiago Tavares <tiagoft@gmail.com>
2025-01-24 13:50:20 -0300

    Added a prototype crawler

--- nlp_course/main.py
+++ nlp_course/main.py
@@ -10,12 +10,24 @@ import typer
 
 import nlp_course as nlp
 
-
 app = typer.Typer(no_args_is_help=True)
 console = Console()
 
+
+@app.command('crawl')
+def crawl_url(
+    url: str,
+    max_documents: int = 10,
+):
+    """
+    Crawl a website and print the visited URLs
+    """
+    _, saved_info = nlp.crawl(url, max_documents)
+    console.print(saved_info)
+
+
 @app.command('info')
-def print_info(custom_message : str = ""):
+def print_info(custom_message: str = ""):
     """
     Print information about the module
     """
@@ -25,7 +37,8 @@ def print_info(custom_message : str = ""):
     if custom_message != "":
         console.print(f"Custom message: {custom_message}")
 
-@app.command('demo') # Defines a default action
+
+@app.command('demo')  # Defines a default action
 def run():
     """
     This is a demonstration function that shows how to use the module
@@ -34,8 +47,10 @@ def run():
     nlp.my_function()
     asset_file = importlib.resources.open_text('nlp.assets', 'poetry.txt')
     print(asset_file.read())
-    asset_file = importlib.resources.open_text('nlp.assets.test_folder', 'test_something.txt')
+    asset_file = importlib.resources.open_text('nlp.assets.test_folder',
+                                               'test_something.txt')
     print(asset_file.read())
+
 
 if __name__ == "__main__":
     app()
